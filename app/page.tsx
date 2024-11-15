f2e3a5191519c4b1ec9807557e6146a291226cc6
Bintang Timurlangit <btimurlangit@gmail.com>
2024-11-15 23:24:15 +0700

    Feat: added Lenis smooth scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect } from 'react';
+import { enableSmoothScroll } from '@/utils/smoothScroll';
 import { Banner } from "@/components/Banner"
 import { Navbar } from "@/components/Navbar"
 import { Hero } from "@/components/Hero"
@@ -8,18 +10,27 @@ import RecentProjects from "@/components/RecentProjects"
 import { Volunteer } from "@/components/Volunteer"
 
 export default function Home() {
+  useEffect(() => {
+    const cleanup = enableSmoothScroll();
+    return () => cleanup();
+  }, []);
+
   return (
     <main className="relative bg-[#010117] flex justify-center items-center flex-col overflow-hidden">
       <div className="w-full items-center">
         <Banner />
         <Navbar />
-        <Hero />
-        <LogoTicker />
+        <div id="hero">
+          <Hero />
+        </div>
+        <div id="logos">
+          <LogoTicker />
+        </div>
       </div>
-      <div className="max-w-[80vw] w-full px-5 sm:px-10">
+      <div className="max-w-[80vw] w-full px-5 sm:px-10" id="projects">
         <RecentProjects />
       </div>
-      <div className="w-full items-center">
+      <div className="w-full items-center" id="volunteer">
         <Volunteer />
       </div>
     </main>
